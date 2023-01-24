48c52993bbabeb09c9a28a7e5e65483fabf9479a
noeliadv <noeliadv@ext.inditex.com>
2023-01-24 13:47:27 -0300

    fix(ci): update scripts of package.json"

--- api/index.js
+++ api/index.js
@@ -3,16 +3,18 @@ const { v4 } = require('uuid')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('../swagger.json')
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.get('/api', (req, res) => {
+app.get('/api/invoices', (req, res) => {
+  console.log(req)
   const path = `/api/invoices/${v4()}`
   res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
 })
 
 app.get('/api/invoices/:slug', (req, res) => {
+  console.log(req)
   const { slug } = req.params
   res.end(`Item: ${slug}`)
 })
