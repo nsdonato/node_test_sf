b6f84159ed7cd769c11027653db92fb88ef8bd4a
noeliadv <noeliadv@ext.inditex.com>
2023-01-24 15:17:32 -0300

    feat(bd): test bd

--- api/index.js
+++ api/index.js
@@ -2,15 +2,52 @@ const app = require('express')()
 const { v4 } = require('uuid')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('../swagger.json')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.get('/api/invoices', (req, res) => {
-  const path = `/api/invoices/${v4()}`
-  res.setHeader('Content-Type', 'text/html')
-  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-})
+async function getAllInvoicesController() {
+  try {
+    const invoices = await prisma.invoice.findMany({
+      select: {
+        id: true,
+        paymentDue: true,
+        status: true,
+        total: true,
+        client: {
+          select: {
+            name: true
+          }
+        }
+      }
+    })
+    return invoices
+  } catch (error) {
+    throw new AppError({ message: String(error), status: 500 })
+  }
+}
+
+async function getAllInvoices(_req, res, next) {
+  try {
+    const invoices = await getAllInvoicesController()
+    res.status(200).json({
+      ok: true,
+      data: invoices
+    })
+  } catch (error) {
+    console.log('getAllInvoices - error: ', error)
+  }
+}
+
+app.get('/api/invoices', getAllInvoices)
+
+// app.get('/api/invoices', (req, res) => {
+//   const path = `/api/invoices/${v4()}`
+//   res.setHeader('Content-Type', 'text/html')
+//   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+// })
 
 app.get('/api/invoices/:slug', (req, res) => {
   const { slug } = req.params
